29f7fa1ced3c0879a3a33a4a0f5626e63c04c687
MarcelHbg <marcel.herzberg@gmx.de>
2026-02-23 08:54:46 +0100

    inital commit

---
+++ radio.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+#firefox https://www.youtube.com/watch?v=36YnV9STBqc
+
+#firefox "https://radiolisten.de/#bob"
+
+#http://streams.radiobob.de/bob-acdc/mp3-192/mediaplayer
+
+#vlc --no-video --open "https://www.youtube.com/watch?v=36YnV9STBqc"
+
+#vlc --open "http://streams.radiobob.de/bob-acdc/mp3-192/mediaplayer"
+
+#vlc --open "http://streams.radiobob.de/bob-national/mp3-192/mediaplayer"
+
+vlc ~/Radio/RadioStreams.xspf
